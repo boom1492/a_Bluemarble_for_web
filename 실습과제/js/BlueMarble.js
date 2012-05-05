4a3ef7177be250b27a3a678793a9e537d8924bb1
Lee junghyun <boom1492@nate.com>
2012-05-05 17:44:24 +0900

    first commit.

---
+++ 실습과제/js/BlueMarble.js
@@ -0,0 +1,282 @@
+var Field_list = new Array(40);
+// Field List Global Variable.
+var Player_list = new Array(4);
+var Player_color = new Array(4);
+var Player_number = [0, 0, 0, 0];
+
+var mTurnNumber = 0;
+var mTurnState = 0;
+var mFund = 0;
+//복지기금
+var STATE_TOLL = 0, STATE_BUYLAND = 1, STATE_BUILD = 2, STATE_GOLDENKEY = 3, STATE_ALONE = 4, STATE_SPACETRIP = 5, STATE_FUND = 6, STATE_RECVFUND = 7, STATE_DICE = 8, STATE_DOUBLE = 9;
+
+function Field(name) {
+    this.td = 0;
+    this.name = name;
+    this.villa = 0;
+    this.building = 0;
+    this.hotel = 0;
+    this.price_villa = 0;
+    this.price_builing = 0;
+    this.price_hotel = 0;
+    this.toll_villa = 0;
+    this.toll_builing = 0;
+    this.toll_hotel = 0;
+    this.toll_default = 0;
+    this.value = 0;
+    this.owner = -1;
+    this.available = 1;
+}
+
+function Player(number, IMEI) {
+    this.number = number;
+    this.location = 0;
+    this.IMEI = IMEI;
+    this.item = {};
+    this.money = 0;
+    this.playerName = "player" + this.number;
+    this.countAlone = 0;
+}
+
+function setName(player, name){
+    Player_list[player].playerName = name;
+}
+
+function test() {
+    //test code
+    joinPlayer("123");
+    joinPlayer("321");
+    movePlayer(0, 5);
+    movePlayer(1, 3);
+    movePlayer(1, 7);
+    joinPlayer("3432");
+    movePlayer(2, 1);
+    joinPlayer("324");
+    movePlayer(3, 10);    
+    startGame();
+}
+
+function joinPlayer(IMEI) {
+    for( i = 0; i < 4; i++) {
+        if(Player_number[i] == 0) {
+            Player_list[i] = new Player(i, IMEI);
+            Player_number[i] = 1;
+            Player_list[i].money = 2000000;
+            $('#log').append("<br/>플레이어" + i + " 접속");
+            redraw(i);
+            return i;
+        }
+    }
+    alert("Full.");
+    return false;
+}
+
+function leavePlayer(number) {
+    Field_list[Player_list[number].location].td.css('background-color', '');
+    Field_list[Player_list[number].location].td.css('opacity', '0.7');
+    Player_list[number] = "";
+    Player_number[number] = 0;
+    $('#log').append("<br/>플레이어" + number + " 퇴장");
+}
+
+function movePlayer(player, index) {
+    Field_list[Player_list[player].location].td.css('background-color', '');
+    Field_list[Player_list[player].location].td.css('opacity', '0.7');
+    Player_list[player].location = index;
+    Field_list[index].td.css('background-color', Player_color[player]);
+    Field_list[index].td.css('opacity', 1);
+
+    $('#log').append("<br/>플레이어" + player + " " + Field_list[index].name + "(으)로 이동");
+    redraw(player);
+    actionNext(player, index);
+    
+}
+
+function actionNext(player, index) {
+    var state = STATE_TOLL;
+    switch(index) {
+        case 2:
+        case 7:
+        case 12:
+        case 17:
+        case 22:
+        case 35:
+            // 황금열쇠
+            state = STATE_GOLDENKEY;
+            break;
+        case 10:
+            //무인도
+            state = STATE_ALONE;
+            break;
+        case 0:
+            //출발
+            break;
+        case 20:
+            state = STATE_RECVFUND;
+            // 사회복지 기금 접수처
+            break;
+        case 38:
+            state = STATE_FUND;
+            // 사회복지기금 내는 곳
+            break;
+        case 30:
+            // 우주여행
+            state = STATE_SPACETRIP;
+            break;
+    }
+    if(state == STATE_FUND) {
+        if(Player_list[player].money < 150000) {
+            //게임오버
+        } else {
+            Player_list[player].money -= 150000;
+            mFund += 150000;
+        }
+    } else if(state == STATE_RECVFUND) {
+        Player_list[player].money += mFund;
+        mFund = 0;
+    } else if(state == STATE_ALONE) {
+        Player_list[player].countAlone++;
+    } else if(state == STATE_GOLDENKEY) {
+
+    } else if(state == STATE_TOLL){
+        if(Field_list[index].owner == -1) {
+            state = STATE_BUYLAND;
+            //구매의사
+            //alert("구매하시겠습니까");
+        } else {
+            var toll = Field_list[index].toll_default;
+            toll += (Field_list[index].villa == 1) ? Field_list[index].toll_villa : 0;
+            toll += (Field_list[index].building == 1) ? Field_list[index].toll_building : 0;
+            toll += (Field_list[index].hotel == 1) ? Field_list[index].toll_hotel : 0;
+
+            if(Player_list[player].money < toll) {
+                //게임 오버
+                alert("게임오버");
+            } else {
+                Player_list[player].money - toll;
+            }
+        }
+
+    }
+    redraw(player);
+}
+function salary(player){
+    Player_list[player].money+=200000;
+    redraw(player);
+}
+
+function buyLand(player, index) {
+    if(Field_list[index].owner != -1) {
+        alert("이미 소유자 있음");
+        return false;
+        // 소유자 있음
+    } else {
+        if(Field_list[index].value > Player_list[player].money) {
+            alert("소지 금액 부족");
+            return false;
+            // 소지 금액 부족
+        } else if(Field_list[index].available == 0) {
+            alert("건설 불가 지역");
+            return false;
+            // 건설 불가 지역
+        } else {
+            Player_list[player].money -= Field_list[index].value;
+            Field_list[index].owner = player;
+            redraw(player);
+            return true;
+        }
+    }
+    return false;
+    // 예상치 못한 예외
+}
+
+function build(player, index, building) {
+    var val = 0;
+    if(building == 0) {
+        val = Field_list[index].price_villa;
+    } else if(building == 1) {
+        val = Field_list[index].price_building;
+    } else if(building == 2) {
+        val = Field_list[index].price_hotel;
+    }
+    if(Field_list[index].owner == player) {
+        if(Player_list[player].money < val) {
+            alert("소유 금액 부족");
+            return false;
+            // 소유 금액 부족
+        } else {
+            switch(building) {
+                case 0:
+                    Field_list[index].villa = 1;
+                    Player_list[player].money -= Field_list[index].price_villa;
+                    break;
+                case 1:
+                    Field_list[index].building = 1;
+                    Player_list[player].money -= Field_list[index].price_building;
+                    break;
+                case 2:
+                    Field_list[index].hotel = 1;
+                    Player_list[player].money -= Field_list[index].price_hotel;
+                    break;
+            }
+        }
+        redraw(player);
+        return true;
+    } else {
+        alert("소유주가 아님 : (소유)" + Field_list[index].owner + "(요구)" + player);
+        return false;
+        // 소유주가 아님
+    }
+    return false;
+    // 예외
+}
+function dice(player, n1, n2){
+    if(n1==n2){
+        Player_list[player].countAlone = 0;
+        mTurnState = STATE_DOUBLE;
+    }
+    movePlayer(player, Player_list[player].location+n1+n2);
+}
+function redraw(player){
+    $('#money_player'+player).html(Player_list[player].money);
+    $('#name_player'+player).html(Player_list[player].playerName);
+    
+    if(Field_list[Player_list[player].location].villa==1){
+        Field_list[Player_list[player].location].td.children().children('.f_bottom').children('.f_bottom_1').css('opacity', 1.0);
+    }
+    if(Field_list[Player_list[player].location].building==1){
+        Field_list[Player_list[player].location].td.children().children('.f_bottom').children('.f_bottom_2').css('opacity', 1.0);
+    }
+    if(Field_list[Player_list[player].location].hotel==1){
+        Field_list[Player_list[player].location].td.children().children('.f_bottom').children('.f_bottom_3').css('opacity', 1.0);
+        
+    }
+}
+function startGame() {
+    var cnt = 0;
+    for( i = 0; i < 4; i++) {
+        if(Player_number == 1) {
+            cnt++;
+        }
+    }
+    mTurnNumber = 0;
+    mTurnState = 0;
+    
+    for(i=0;i<4;i++){
+        $('#money_player'+i).html(Player_list[i].money);
+    }
+
+}
+
+
+$(document).ready(function() {
+    Player_color = ['red', 'blue', 'green', 'yellow'];
+    init();
+    //test();
+    $('#testBox').keydown(function(e) {
+    if(e.keyCode == 13)
+        eval($('#testBox').val());
+    });
+    
+});
+
---
+++ 실습과제/js/Init.js
@@ -0,0 +1,389 @@
+function init(){    
+    var mHeight = document.body.clientHeight;
+    var mWidth = document.body.clientWidth;
+    var Field_table_list = $('#boardtable td:not(.dummy)');
+     // 필드 크기 초기화
+    Field_table_list.css('width',mWidth/10);
+    Field_table_list.css('height',(mHeight-40)/11);
+
+    Field_list[0] = new Field("출발");
+    Field_list[0].td = Field_table_list.eq(0);
+
+    Field_list[1] = new Field("중국 타이페이");
+    Field_list[1].td = Field_table_list.eq(1);
+    Field_list[1].value = 180000;
+    Field_list[1].toll_default = 20000;
+    Field_list[1].toll_villa = 40000;
+    Field_list[1].toll_building = 80000;
+    Field_list[1].toll_hotel = 150000;
+    Field_list[1].price_villa = 35000;
+    Field_list[1].price_building = 70000;
+    Field_list[1].price_hotel = 150000;
+    
+    Field_list[2] = new Field("황금열쇠");
+    Field_list[2].td = Field_table_list.eq(2);
+
+    Field_list[3] = new Field("홍콩");
+    Field_list[3].td = Field_table_list.eq(3);
+    Field_list[3].value = 300000;
+    Field_list[3].toll_default = 50000;
+    Field_list[3].toll_villa = 80000;
+    Field_list[3].toll_building = 100000;
+    Field_list[3].toll_hotel = 200000;
+    Field_list[3].price_villa = 60000;
+    Field_list[3].price_building = 90000;
+    Field_list[3].price_hotel = 190000;
+
+    Field_list[4] = new Field("필리핀 마닐라");
+    Field_list[4].td = Field_table_list.eq(4);
+    Field_list[4].value = 120000;
+    Field_list[4].toll_default = 15000;
+    Field_list[4].toll_villa = 30000;
+    Field_list[4].toll_building = 60000;
+    Field_list[4].toll_hotel = 90000;
+    Field_list[4].price_villa = 29000;
+    Field_list[4].price_building = 55000;
+    Field_list[4].price_hotel = 80000;
+    
+    
+    Field_list[5] = new Field("대한민국 제주도");
+    Field_list[5].td = Field_table_list.eq(5);
+    Field_list[5].value = 500000;
+    Field_list[5].toll_default = 150000;
+
+    Field_list[6] = new Field("싱가포르");
+    Field_list[6].td = Field_table_list.eq(6);
+    Field_list[6].value = 220000;
+    Field_list[6].toll_default = 30000;
+    Field_list[6].toll_villa = 40000;
+    Field_list[6].toll_building = 80000;
+    Field_list[6].toll_hotel = 240000;
+    Field_list[6].price_villa = 35000;
+    Field_list[6].price_building = 70000;
+    Field_list[6].price_hotel = 230000;
+    
+
+    Field_list[7] = new Field("황금열쇠");
+    Field_list[7].td = Field_table_list.eq(7);
+
+    Field_list[8] = new Field("이집트 카이로");
+    Field_list[8].td = Field_table_list.eq(8);
+    Field_list[8].value = 80000;
+    Field_list[8].toll_default = 20000;
+    Field_list[8].toll_villa = 90000;
+    Field_list[8].toll_building = 130000;
+    Field_list[8].toll_hotel = 200000;
+    Field_list[8].price_villa = 80000;
+    Field_list[8].price_building = 110000;
+    Field_list[8].price_hotel = 180000;
+    
+    Field_list[9] = new Field("터키 이스탄불");
+    Field_list[9].td = Field_table_list.eq(9);
+    Field_list[9].value = 300000;
+    Field_list[9].toll_default = 60000;
+    Field_list[9].toll_villa = 100000;
+    Field_list[9].toll_building = 180000;
+    Field_list[9].toll_hotel = 250000;
+    Field_list[9].price_villa = 90000;
+    Field_list[9].price_building = 160000;
+    Field_list[9].price_hotel = 230000;
+    
+    Field_list[10] = new Field("무인도");
+    Field_list[10].td = Field_table_list.eq(10);
+
+    Field_list[11] = new Field("그리스 아테네");
+    Field_list[11].td = Field_table_list.eq(12);
+    Field_list[11].value = 120000;
+    Field_list[11].toll_default = 50000;
+    Field_list[11].toll_villa = 20000;
+    Field_list[11].toll_building = 40000;
+    Field_list[11].toll_hotel = 80000;
+    Field_list[11].price_villa = 2000;
+    Field_list[11].price_building = 40000;
+    Field_list[11].price_hotel = 80000;
+    
+    Field_list[12] = new Field("황금열쇠");
+    Field_list[12].td = Field_table_list.eq(14);
+
+    Field_list[13] = new Field("덴마크 코펜하겐");
+    Field_list[13].td = Field_table_list.eq(16);
+    Field_list[13].value = 270000;
+    Field_list[13].toll_default = 100000;
+    Field_list[13].toll_villa = 50000;
+    Field_list[13].toll_building = 130000;
+    Field_list[13].toll_hotel = 240000;
+    Field_list[13].price_villa = 4000;
+    Field_list[13].price_building = 110000;
+    Field_list[13].price_hotel = 220000;
+    
+    Field_list[14] = new Field("스웨덴 스톡홀름");
+    Field_list[14].td = Field_table_list.eq(18);
+    Field_list[14].value = 340000;
+    Field_list[14].toll_default = 90000;
+    Field_list[14].toll_villa = 40000;
+    Field_list[14].toll_building = 150000;
+    Field_list[14].toll_hotel = 300000;
+    Field_list[14].price_villa = 80000;
+    Field_list[14].price_building = 150000;
+    Field_list[14].price_hotel = 310000;
+    
+    Field_list[15] = new Field("콩코드 여객기");
+    Field_list[15].td = Field_table_list.eq(20);
+    Field_list[15].toll_default = 50000;
+    Field_list[15].value = 150000;
+    
+
+    Field_list[16] = new Field("스위스 취리히");
+    Field_list[16].td = Field_table_list.eq(22);
+    Field_list[16].value = 310000;
+    Field_list[16].toll_default = 70000;
+    Field_list[16].toll_villa = 70000;
+    Field_list[16].toll_building = 120000;
+    Field_list[16].toll_hotel = 200000;
+    Field_list[16].price_villa = 60000;
+    Field_list[16].price_building = 120000;
+    Field_list[16].price_hotel = 220000;
+    
+
+    Field_list[17] = new Field("황금열쇠");
+    Field_list[17].td = Field_table_list.eq(24);
+
+    Field_list[18] = new Field("독일 베를린");
+    Field_list[18].td = Field_table_list.eq(26);
+    Field_list[18].value = 400000;
+    Field_list[18].toll_default = 100000;
+    Field_list[18].toll_villa = 100000;
+    Field_list[18].toll_building = 200000;
+    Field_list[18].toll_hotel = 300000;
+    Field_list[18].price_villa = 80000;
+    Field_list[18].price_building = 190000;
+    Field_list[18].price_hotel = 300000;
+    
+
+    Field_list[19] = new Field("캐나다 몬트리올");
+    Field_list[19].td = Field_table_list.eq(28);
+    Field_list[19].value = 200000;
+    Field_list[19].toll_default = 80000;
+    Field_list[19].toll_villa = 30000;
+    Field_list[19].toll_building = 100000;
+    Field_list[19].toll_hotel = 180000;
+    Field_list[19].price_villa = 28000;
+    Field_list[19].price_building = 95000;
+    Field_list[19].price_hotel = 178000;
+    
+
+    Field_list[20] = new Field("사회복지기금 접수처");
+    Field_list[20].td = Field_table_list.eq(39);
+
+    Field_list[21] = new Field("아르헨티나 부에노스아이레스");
+    Field_list[21].td = Field_table_list.eq(38);
+    Field_list[21].value = 100000;
+    Field_list[21].toll_default = 40000;
+    Field_list[21].toll_villa = 20000;
+    Field_list[21].toll_building = 80000;
+    Field_list[21].toll_hotel = 130000;
+    Field_list[21].price_villa = 20000;
+    Field_list[21].price_building = 80000;
+    Field_list[21].price_hotel = 140000;
+    
+
+    Field_list[22] = new Field("황금열쇠");
+    Field_list[22].td = Field_table_list.eq(37);
+
+    Field_list[23] = new Field("브라질 상파울루");
+    Field_list[23].td = Field_table_list.eq(36);
+    Field_list[23].value = 250000;
+    Field_list[23].toll_default = 50000;
+    Field_list[23].toll_villa = 40000;
+    Field_list[23].toll_building = 100000;
+    Field_list[23].toll_hotel = 200000;
+    Field_list[23].price_villa = 35000;
+    Field_list[23].price_building = 100000;
+    Field_list[23].price_hotel = 210000;
+    
+
+    Field_list[24] = new Field("오스트레일리아 시드니");
+    Field_list[24].td = Field_table_list.eq(35);
+    Field_list[24].value = 130000;
+    Field_list[24].toll_default = 44000;
+    Field_list[24].toll_villa = 45000;
+    Field_list[24].toll_building = 95000;
+    Field_list[24].toll_hotel = 120000;
+    Field_list[24].price_villa = 40000;
+    Field_list[24].price_building = 90000;
+    Field_list[24].price_hotel = 120000;
+    
+
+    Field_list[25] = new Field("대한민국 부산");
+    Field_list[25].td = Field_table_list.eq(34);
+    Field_list[25].toll_default = 200000;
+    Field_list[25].value = 700000;
+    
+    Field_list[26] = new Field("미국 하와이");
+    Field_list[26].td = Field_table_list.eq(33);
+    Field_list[26].value = 500000;
+    Field_list[26].toll_default = 100000;
+    Field_list[26].toll_villa = 100000;
+    Field_list[26].toll_building = 200000;
+    Field_list[26].toll_hotel = 400000;
+    Field_list[26].price_villa = 90000;
+    Field_list[26].price_building = 200000;
+    Field_list[26].price_hotel = 380000;
+    
+
+    Field_list[27] = new Field("포르투갈 리스본");
+    Field_list[27].td = Field_table_list.eq(32);
+    Field_list[27].value = 120000;
+    Field_list[27].toll_default = 50000;
+    Field_list[27].toll_villa = 40000;
+    Field_list[27].toll_building = 130000;
+    Field_list[27].toll_hotel = 230000;
+    Field_list[27].price_villa = 30000;
+    Field_list[27].price_building = 120000;
+    Field_list[27].price_hotel = 230000;
+    
+
+    Field_list[28] = new Field("퀸 엘리자베스호");
+    Field_list[28].td = Field_table_list.eq(31);
+    Field_list[28].toll_default = 100000;
+    Field_list[28].value = 300000;
+    
+    
+    Field_list[29] = new Field("스페인 마드리드");
+    Field_list[29].td = Field_table_list.eq(30);
+    Field_list[29].value = 90000;
+    Field_list[29].toll_default = 20000;
+    Field_list[29].toll_villa = 50000;
+    Field_list[29].toll_building = 70000;
+    Field_list[29].toll_hotel = 120000;
+    Field_list[29].price_villa = 50000;
+    Field_list[29].price_building = 70000;
+    Field_list[29].price_hotel = 130000;
+    
+
+    Field_list[30] = new Field("우주여행");
+    Field_list[30].td = Field_table_list.eq(29);
+
+    Field_list[31] = new Field("일본 도쿄");
+    Field_list[31].td = Field_table_list.eq(27);
+    Field_list[31].value = 180000;
+    Field_list[31].toll_default = 50000;
+    Field_list[31].toll_villa = 100000;
+    Field_list[31].toll_building = 150000;
+    Field_list[31].toll_hotel = 300000;
+    Field_list[31].price_villa = 70000;
+    Field_list[31].price_building = 130000;
+    Field_list[31].price_hotel = 280000;
+    
+
+    Field_list[32] = new Field("컬럼비아 호");
+    Field_list[32].td = Field_table_list.eq(25);
+    Field_list[32].toll_default = 150000;
+    Field_list[32].value = 300000;
+    
+    Field_list[33] = new Field("프랑스 파리");
+    Field_list[33].td = Field_table_list.eq(23);
+    Field_list[33].value = 280000;
+    Field_list[33].toll_default = 100000;
+    Field_list[33].toll_villa = 90000;
+    Field_list[33].toll_building = 200000;
+    Field_list[33].toll_hotel = 350000;
+    Field_list[33].price_villa = 95000;
+    Field_list[33].price_building = 210000;
+    Field_list[33].price_hotel = 380000;
+    
+
+    Field_list[34] = new Field("이탈리아 로마");
+    Field_list[34].td = Field_table_list.eq(21);
+    Field_list[34].value = 80000;
+    Field_list[34].toll_default = 30000;
+    Field_list[34].toll_villa = 50000;
+    Field_list[34].toll_building = 90000;
+    Field_list[34].toll_hotel = 170000;
+    Field_list[34].price_villa = 45000;
+    Field_list[34].price_building = 90000;
+    Field_list[34].price_hotel = 180000;
+    
+
+    Field_list[35] = new Field("황금열쇠");
+    Field_list[35].td = Field_table_list.eq(19);
+
+    Field_list[36] = new Field("영국 런던");
+    Field_list[36].td = Field_table_list.eq(17);
+    Field_list[36].value = 340000;
+    Field_list[36].toll_default = 150000;
+    Field_list[36].toll_villa = 90000;
+    Field_list[36].toll_building = 250000;
+    Field_list[36].toll_hotel = 400000;
+    Field_list[36].price_villa = 8000;
+    Field_list[36].price_building = 230000;
+    Field_list[36].price_hotel = 350000;
+    
+
+    Field_list[37] = new Field("미국 뉴욕");
+    Field_list[37].td = Field_table_list.eq(15);
+    Field_list[37].value = 150000;
+    Field_list[37].toll_default = 120000;
+    Field_list[37].toll_villa = 40000;
+    Field_list[37].toll_building = 100000;
+    Field_list[37].toll_hotel = 320000;
+    Field_list[37].price_villa = 4000;
+    Field_list[37].price_building = 110000;
+    Field_list[37].price_hotel = 330000;
+    
+
+    Field_list[38] = new Field("사회복지기금 내는 곳");
+    Field_list[38].td = Field_table_list.eq(13);
+
+    Field_list[39] = new Field("대한민국 서울");
+    Field_list[39].td = Field_table_list.eq(11);
+    Field_list[39].toll_default = 300000;
+    Field_list[39].value = 1000000;
+    
+    for(i=0;i<Field_list.length;i++){
+        var str = "<div class='Field'><div class='f_top'></div>"
+                    +"<div class='f_body'>"
+                    +"<div class='f_body_name'>"
+                    +Field_list[i].name
+                    +"</div>"
+                    +"</div>";
+            
+        switch(i){
+            case 0:
+            case 2:
+            case 7:
+            case 10:
+            case 12:
+            case 15:
+            case 17:
+            case 20:
+            case 22:
+            case 30:
+            case 35:
+            case 38:
+            case 5:
+            case 25:
+            case 39:
+            case 28:
+            case 32:    Field_list[i].available = 0; break;
+            default: str += "<div class='f_bottom'>"+
+                "<span class='f_bottom_1'>●</span>"+
+                "<span class='f_bottom_2'>●</span>"+
+                "<span class='f_bottom_3'>●</span>"+
+                "</div>";
+                break;
+        }
+        str += "</div>";
+        Field_list[i].td.html(str);
+    }    
+    $('.f_bottom_1').css('height', (mHeight-40)/33);
+    $('.f_bottom_2').css('height', (mHeight-40)/33);
+    $('.f_bottom_3').css('height', (mHeight-40)/33);
+    
+    $('.f_bottom_1').css('opacity', '0.1');
+    $('.f_bottom_2').css('opacity', '0.1');
+    $('.f_bottom_3').css('opacity', '0.1');
+    
+    $('#initPage').show();
+    $('#body').hide();
+}
